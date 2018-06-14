a04144787c66e5d6fa779d83825701e6fc451513
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-06-14 02:51:37 -0700

    setuptodolist

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Todo extends Component {
   render() {
-    return <li>item</li>;
+    const { id, completed, description, toggleComplete } = this.props;
+    return (
+      <li>
+        <input
+          className="uk-checkbox"
+          type="checkbox"
+          checked={completed}
+          onChange={e => toggleComplete(e.target.completed, id)}
+        />
+        {description}
+      </li>
+    );
   }
 }
+
+Todo.propTypes = {
+  id: PropTypes.number.isRequired,
+  completed: PropTypes.bool.isRequired,
+  description: PropTypes.string.isRequired,
+  toggleComplete: PropTypes.func.isRequired
+};
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import axios from "axios";
 
 export default class TodoList extends Component {
+  state = {
+    todos: []
+  };
+  componentDidMount = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(result => this.setState({ todos: result.data }))
+      .catch(err => {
+        console.error("could not fetch todos");
+      });
+  };
+
+  toggleComplete = (bool, id) => {
+    this.setState(prevState => {
+      const todos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return Object.assign({}, todo, { completed: bool });
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        <ul> {[1, 2, 3].map(todo => <Todo key={todo} />)} </ul>
+        <ul className="uk-list">
+          {todos.map(todo => (
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              description={todo.title}
+              completed={todo.completed}
+              toggleComplete={this.toggleComplete}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
