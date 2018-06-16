ef076f2c4b6d6ee0911c71aaed6312718bad4289
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-06-16 01:40:46 -0700

    checkpoint

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,12 +4,14 @@ import PropTypes from "prop-types";
 const Search = ({ handleSearch, filter }) => {
   return (
     <form className="uk-search uk-search-default">
+      <a href="" uk-search-icon="" />
       <input
-        className="uk-search-input uk-form-small"
+        className="uk-search-input uk-form-small uk-form-icon uk-form-icon-flip"
         type="search"
         placeholder="Search..."
         onChange={e => handleSearch(e.target.value)}
         value={filter}
+        uk-icon="icon: close"
       />
     </form>
   );
